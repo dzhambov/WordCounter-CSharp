8ba6a31c6af5e99d2590854b1848aaf06ea74a12
Student <example@example.com>
2020-03-06 16:52:57 -0800

    add user inteface to WordCount project.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -4,10 +4,39 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
-    public static string UserWord(string word)
+    public string UserWord { get; set; }
+    public string UserSentence { get; set; }
+
+    public void UserWordCounter(string word, string sentence)
     {
-      
+      UserWord = word;
+      UserSentence = sentence;
+      int countWord = 0;
+      // foreach(string sent in sentence)
+      // {
+      //   countWord++;
+      // }
+      // return countWord;
     }
+
+    public string SampleWord(string word)
+    {
+      string inputWord = word;
+      return inputWord;
+    }
+
+    public string SampleSentence( string sentence)
+    {
+      string inputSentence = sentence;
+      return inputSentence;
+    }
+
+    public static string[] SplitSentence(string sentence)
+    {
+      return sentence.Split(" ");
+    }
+    
+    
 
   }
 }
--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,8 +7,25 @@ namespace WordCounter
   {
     public static void Main()
     {
-     Console.WriteLine();
+     Console.Clear();
+     Console.ForegroundColor = ConsoleColor.DarkYellow;
+     TypeLine("Hello Word Contre");
+     TypeLine("To start playing first enter a word");
+     string word = Console.ReadLine();
+     Console.ForegroundColor = ConsoleColor.DarkGreen;
+     TypeLine("Now enter a sentence");
+     string sentence = Console.ReadLine();
     }
+    public static void TypeLine(string input)
+      {
+      for (int i = 0; i < input.Length; i++)
+      {
+          Console.Write(input[i]);
+          System.Threading.Thread.Sleep(35);
+      }
+      Console.Write(Environment.NewLine);
+      Console.Write(Environment.NewLine);
+      }
   }
 
 }
